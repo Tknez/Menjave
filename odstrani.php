49c98bdb92a19f6d84238120927b35a0cf99bea1
Armin Komić <armkom@gmail.com>
2016-05-15 05:19:05 +0000

    Brisanje DELA!

--- odstrani.php
+++ odstrani.php
@@ -2,9 +2,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <?php
-
+	include ("connect.php");
 	$id = $_GET['id'];
-
 	if(isset($_GET['id']))
 	{
 		mysql_query("DELETE FROM ponudba WHERE idponudba='$id'");
